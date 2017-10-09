b0e9795579c05b3871e39f2935d451a3d50e8eb4
boyned//Kampfkarren <boynedmaster@gmail.com>
2017-10-08 17:28:21 -0700

    Create GetMassOfModel.module.lua

---
+++ GetMassOfModel.module.lua
@@ -0,0 +1,11 @@
+return function(model)
+	local mass = 0
+	
+	for _,object in pairs(model:GetDescendants()) do
+		if object:IsA("BasePart") then
+			mass = mass + object:GetMass()
+		end
+	end
+	
+	return mass
+end
