e489597925bc65a1f08d2a8c12289c684e60fa73
boyned//Kampfkarren <boynedmaster@gmail.com>
2017-10-08 17:40:41 -0700

    Create Iterators.module.lua

---
+++ Iterators.module.lua
@@ -0,0 +1,21 @@
+local Iterators = {}
+
+Iterators.shuffle = function(t)
+  local keyList = {}
+  
+  for i,_ in pairs(t) do
+    table.insert(keyList, i)
+  end
+  
+  return function(t)
+    if #keyList == 0 then
+      return nil
+    end
+    
+    local randomValue = table.remove(keyList, math.random(#keyList))
+    
+    return randomValue, t[randomValue]
+  end, t, 0
+end
+
+return Iterators
