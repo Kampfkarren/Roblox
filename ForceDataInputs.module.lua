235cca1fdd22be509f2f3422da611e3d02e30864
boyned//Kampfkarren <boynedmaster@gmail.com>
2017-12-06 06:54:51 -0800

    Fix vulnerability

--- ForceDataInputs.module.lua
+++ ForceDataInputs.module.lua
@@ -1,6 +1,6 @@
 return function(data, ply_to_kick, force)
-	for i,v in pairs(data) do
-		if typeof(v) ~= force[i] then
+	for i,v in pairs(force) do
+		if v ~= typeof(data[i]) then
 			warn("invalid data inputs")
 			ply_to_kick:Kick(0x02)
 			return false
