fb5a62c52965594b5fd63af0398f9f12cc2fb426
boyned//Kampfkarren <boynedmaster@gmail.com>
2017-12-21 07:24:37 -0800

    Create CommaSeparated.lua

---
+++ Snippets/CommaSeparated.lua
@@ -0,0 +1,12 @@
+local function commaSeparated(list)
+	if #list == 1 then
+		return tostring(list[1])
+	elseif #list == 2 then
+		return list[1] .. " and " .. list[2]
+	else
+		local lastItem = table.remove(list)
+		local text = table.concat(list, ", ") .. ", and " .. lastItem
+		table.insert(list, lastItem)
+		return text
+	end
+end
