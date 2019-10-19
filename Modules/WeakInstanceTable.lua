a21800c8263e1fadfcc04e90d8dfd58a517a02e6
howmanysmall <lhamil29@bruinmail.slcc.edu>
2019-10-19 13:31:43 -0600

    __tostring has to be a function (?)
    Also a little faster I believe.

--- Modules/WeakInstanceTable.lua
+++ Modules/WeakInstanceTable.lua
@@ -1,17 +1,20 @@
-local weakInstanceTable = {}
-weakInstanceTable.__tostring = "<Weak Instance Table>"
+local weakInstanceTable = {
+	__tostring = function()
+		return "<Weak Instance Table>"
+	end,
 
-function weakInstanceTable:__newindex(key, value)
-	assert(typeof(key) == "Instance", "keys need to be an Instance")
-	assert(key:IsDescendantOf(game), "key is not a descendant of the DataModel")
-	rawset(self, key, value)
+	__newindex = function(self, key, value)
+		assert(typeof(key) == "Instance", "keys need to be an Instance")
+		assert(key:IsDescendantOf(game), "key is not a descendant of the DataModel")
+		rawset(self, key, value)
 
-	key.AncestryChanged:connect(function()
-		if not key:IsDescendantOf(game) then
-			rawset(self, key, nil)
-		end
-	end)
-end
+		key.AncestryChanged:Connect(function()
+			if not key:IsDescendantOf(game) then
+				rawset(self, key, nil)
+			end
+		end)
+	end,
+}
 
 return function()
 	return setmetatable({}, weakInstanceTable)
